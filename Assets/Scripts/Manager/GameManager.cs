9988afa2740a23fcae8ad6cafd2fee897acf4f3c
MSKim0215 <rlaalstjqu05@gmail.com>
2025-01-18 01:21:40 +0900

    게임 매니저 세팅

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,19 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MSKim.Manager
 {
     public class GameManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [Header("Settings")]
+        [SerializeField] private List<GameObject> waitChairList = new();
+        [SerializeField] private List<GameObject> pickupTableList = new();
 
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        [Header("Info Viewer")]
+        [SerializeField] private int currentWaitNumber;
+        [SerializeField] private Queue<NonPlayer.GuestController> pickupZoneGuests = new();
+        [SerializeField] private Queue<NonPlayer.GuestController> waitingZoneGuests = new();
     }
 }
