13f4edf697461adf916fdcc7e2f8af38358b468a
MSKim0215 <rlaalstjqu05@gmail.com>
2025-01-17 21:22:18 +0900

    손님 웨이포인트 매니저 추가

---
+++ Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSKim.Manager
+{
+    public class WaypointManager : MonoBehaviour 
+    {
+        private static WaypointManager instance;
+
+        public static WaypointManager Instance
+        {
+            get
+            {
+                if (instance == null) instance = new();
+                return instance;
+            }
+        }
+
+        [Header("Guest Waypoint Settings")]
+        [SerializeField] private List<Transform> wayPointList = new();
+
+        private void Awake()
+        {
+            if(instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public Vector3 GetCurrentWayPoint(int currentIndex) => wayPointList[currentIndex].position;
+    }
+}
