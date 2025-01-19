215e24c9228c4ff455c208cbb48bf07860d7e0c8
MSKim0215 <rlaalstjqu05@gmail.com>
2025-01-19 17:10:55 +0900

    스튜 스크립트 추가

---
+++ Assets/Scripts/HandAble/Food/StewFoodController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MSKim.HandAble
+{
+    public class StewFoodController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
