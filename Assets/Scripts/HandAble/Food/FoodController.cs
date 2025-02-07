853b7d22e59acfe41bc3e2ee67643827f756462f
MSKim0215 <rlaalstjqu05@gmail.com>
2025-02-08 04:28:37 +0900

    음식 포장 시, 접시 생성 구현

--- Assets/Scripts/HandAble/Food/FoodController.cs
+++ Assets/Scripts/HandAble/Food/FoodController.cs
@@ -8,12 +8,20 @@ namespace MSKim.HandAble
         [Header("Food Data Info")]
         [SerializeField] protected Data.FoodData data;
 
+        [Header("Plate")]
+        [SerializeField] protected GameObject plate;
+
         public Utils.FoodState CurrentFoodState { get; set; }
 
         public virtual void Initialize(Utils.FoodType foodType)
         {
             data = Managers.GameData.GetFoodData(foodType);
             name = data.Name;
+        }
+
+        public void Packaing()
+        {
+            plate.SetActive(true);
         }
     }
 }
--- Assets/Scripts/HandNotAble/Table/PackagingTableController.cs
+++ Assets/Scripts/HandNotAble/Table/PackagingTableController.cs
@@ -11,28 +11,15 @@ namespace MSKim.HandNotAble
             name = data.Name;
         }
 
-        private bool isPackaging = false;
-
         public override void Take(GameObject takeObject)
         {
             if(takeObject.TryGetComponent<HandAble.FoodController>(out var food))
             {
                 food.CurrentFoodState = Utils.FoodState.Packaging;
+                food.Packaing();
             }
 
             base.Take(takeObject);
-        }
-
-        public void Packaging()
-        {
-            if (isPackaging)
-            {
-                Debug.Log("포장 완료");
-                return;
-            }
-            isPackaging = true;
-
-            Debug.Log("포장 중!!!");
         }
     }
 }
